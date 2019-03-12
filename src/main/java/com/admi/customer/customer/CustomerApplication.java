9857ce9e5c9e69479c744ff96974b728f638fd64
Utpal <Utbhatta@in.ibm.com>
2019-03-12 16:27:06 +0530

    added SpringBootServletInitializer

--- src/main/java/com/admi/customer/customer/CustomerApplication.java
+++ src/main/java/com/admi/customer/customer/CustomerApplication.java
@@ -2,9 +2,10 @@ package com.admi.customer.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class CustomerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
